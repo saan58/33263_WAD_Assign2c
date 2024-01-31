de424d006fc7946db4c0e8cca0c1c281bc7ff5c2
saan58 <rithe.sanika@gmail.com>
2024-01-31 11:59:54 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ShowusersComponent } from './showusers/showusers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "register",
+    component:RegistrationComponent
+  },
+  {
+    path:"login",
+    component: LoginComponent
+  },
+  {
+    path:"",
+    component: HomeComponent
+  },
+  {
+    path:"showusers",
+    component: ShowusersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
